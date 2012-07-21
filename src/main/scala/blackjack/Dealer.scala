fe0732a09be6915f97d3ceef18a5f3daa6852715
Rhys Jones <rjones.zx@gmail.com>
2012-07-21 17:07:22 +0100

    dealer file added

---
+++ src/main/scala/blackjack/Dealer.scala
@@ -0,0 +1,11 @@
+package blackjack
+
+case class Dealer(cards: Seq[Card], shoe: Shoe)(implicit rules: Rules) extends HandScores {
+  
+  def deal = copy(cards = cards :+ shoe.card, shoe = shoe.next)
+  
+  def traverse: Dealer = 
+    if (score < 17 || score == 17 && rules.HIT_SOFT_17) deal.traverse
+    else this
+  
+}
